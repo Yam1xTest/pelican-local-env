53e3fcfdd92a705af98f080142ea83497afe046a
Yam1x <rychkov03@yandex.ru>
2025-03-13 10:46:44 +0000

    feat: zip archive for backups && datetime

--- s3-backup.py
+++ s3-backup.py
@@ -1,14 +1,21 @@
 import rclone
 import os
+import shutil
+from datetime import datetime
 def main():
 
     cfg_path = r'.rclone.conf'
 
     with open(cfg_path) as f:
         cfg = f.read()
+        
+    rclone.with_config(cfg).copy("minio:pelican-local-env", "/tmp/s3-backup", flags=["--transfers=256"])
 
-    result = rclone.with_config(cfg).copy("minio:pelican-local-env", "minio:123/test2", flags=["--transfers=256"])
+    archive_name = "s3-backup" + '_' + datetime.now().strftime("%Y.%m.%d.%H-%M") + 'UTC'
 
+    shutil.make_archive(archive_name, 'zip', "/tmp/s3-backup")
+
+    rclone.with_config(cfg).copy(archive_name + ".zip", "minio:backup", flags=["--transfers=256"])
 if __name__ == '__main__':
 
     main()
